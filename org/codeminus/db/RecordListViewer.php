d2108505074b87b9db135a9e87bcf743735ccc9f
Marcel Willian <web@marcelwillian.com>
2013-05-22 09:45:39 -0300

    db\RecordListViewer completely re-implemented

--- org/codeminus/db/RecordListViewer.php
+++ org/codeminus/db/RecordListViewer.php
@@ -2,26 +2,21 @@
 
 namespace org\codeminus\db;
 
-use org\codeminus\util as util;
-
 /**
  * Database record list viewer
  * Used along with RecordList class creates paging
  * @author Wilson Santos <wilson@codeminus.org>
- * @version 0.2b
+ * @version 0.3
  */
 class RecordListViewer {
 
   private $recordList;
-  private $dictionary;
   private $linkSource;
   private $pageVarName = 'pg';
-  private $linkVars;
-  private $firstPageButtonImage;
-  private $previousPageButtonImage;
-  private $nextPageButtonImage;
-  private $lastPageButtonImage;
-  private $defaultButtonStyle = 'style="margin-bottom: -2px"';
+  private $firstPageButtonContent = '|&lt;';
+  private $previousPageButtonContent = '&lt;';
+  private $nextPageButtonContent = '&gt;';
+  private $lastPageButtonContent = '&gt;|';
 
   const COMPLETE_CONTROLS = 0;
   const SIMPLE_CONTROLS = 1;
@@ -32,9 +27,8 @@ class RecordListViewer {
    * @param string $language used for viewer translation
    * @return RecordListViewer
    */
-  public function __construct(RecordList $recordList, $language = util\Dictionary::LANGUAGE_PTBR) {
+  public function __construct(RecordList $recordList) {
     $this->setRecordList($recordList);
-    $this->dictionary = new util\Dictionary($language);
   }
 
   /**
@@ -97,44 +91,6 @@ class RecordListViewer {
   }
 
   /**
-   * 
-   * ATTENTION: this method does not work with codeminus MVC framework
-   * 
-   * GET query string to be sent along with the paging variable
-   * @return string
-   */
-  /* public function getLinkVars() {
-
-    if(!isset($this->linkVars)){
-    if(count($_REQUEST) > 0){
-    $linkVars = "";
-    foreach(array_keys($_REQUEST) as $key){
-    if($key != $this->getPageVarName()){
-    $linkVars .= '&'.$key.'='.$_REQUEST[$key];
-    }
-    }
-    $this->setLinkVars($linkVars);
-    }
-    }
-
-    return $this->linkVars;
-
-    } */
-
-  /**
-   * GET query string to be sent along with the paging variable.
-   * This method is useful to avoid GET querystring injection
-   * @param string $linkVars in querystring format.
-   * DO NOT pass the variable that is already been used for paging control.
-   * It will be set automatically.
-   * @return void
-   * @example setLinkVars('mode=fast&orderby=name');
-   */
-  /* public function setLinkVars($linkVars) {
-    $this->linkVars = $linkVars;
-    } */
-
-  /**
    * Page complete link source
    * @param int $page the destination page 
    * @return string
@@ -150,136 +106,71 @@ class RecordListViewer {
   }
 
   /**
-   * Image tag with image representing the first page
+   * 
    * @return string
    */
-  public function getFirstPageButtonImage() {
-
-    if (isset($this->firstPageButtonImage)) {
-
-      return $this->firstPageButtonImage;
-    } else {
-
-      $source = LIB_ICONS_PATH . '/control_first.png';
-      $alt = $this->dictionary->text(util\Dictionary::FIRST_PAGE);
-      return util\HTML::image($source, $alt, $this->getDefaultButtonStyle());
-    }
+  public function getFirstPageButtonContent() {
+    return $this->firstPageButtonContent;
   }
 
   /**
-   * Image tag with image representing the first page
-   * @param string $source image location
-   * @param string $alt image alternative text
+   * 
+   * @param string $content
    * @return void
    */
-  public function setFirstPageButtonImage($source, $alt = null) {
-    if (!isset($alt))
-      $alt = $this->dictionary->text(util\Dictionary::FIRST_PAGE);
-    $this->firstPageButtonImage = util\HTML::image($source, $alt);
+  public function setFirstPageButtonContent($content) {
+    $this->firstPageButtonContent = $content;
   }
 
   /**
-   * Image tag with image representing the previous page
+   * 
    * @return string
    */
-  public function getPreviousPageButtonImage() {
-
-    if (isset($this->previousPageButtonImage)) {
-
-      return $this->previousPageButtonImage;
-    } else {
-
-      $source = LIB_ICONS_PATH . '/control_prev.png';
-      $alt = $this->dictionary->text(util\Dictionary::PREV_PAGE);
-      return util\HTML::image($source, $alt, $this->getDefaultButtonStyle());
-    }
+  public function getPreviousPageButtonContent() {
+    return $this->previousPageButtonContent;
   }
 
   /**
-   * Image tag with image representing the previous page
-   * @param string $source image location
-   * @param string $alt image alternative text
+   * 
+   * @param string $content
    * @return void
    */
-  public function setPreviousPageButtonImage($source, $alt = null) {
-    if (!isset($alt))
-      $alt = $this->dictionary->text(util\Dictionary::PREV_PAGE);
-    $this->previousPageButtonImage = util\HTML::image($source, $alt);
+  public function setPreviousPageButtonContent($content) {
+    $this->previousPageButtonContent = $content;
   }
 
   /**
-   * Image tag with image representing the next page
+   * 
    * @return string
    */
-  public function getNextPageButtonImage() {
-
-    if (isset($this->nextPageButtonImage)) {
-
-      return $this->nextPageButtonImage;
-    } else {
-
-      $source = LIB_ICONS_PATH . '/control_next.png';
-      $alt = $this->dictionary->text(util\Dictionary::NEXT_PAGE);
-
-      return util\HTML::image($source, $alt, $this->getDefaultButtonStyle());
-    }
+  public function getNextPageButtonContent() {
+    return $this->nextPageButtonContent;
   }
 
   /**
-   * Image tag with image representing the next page
-   * @param string $source image location
-   * @param string $alt image alternative text
+   * 
+   * @param string $content
    * @return void
    */
-  public function setNextPageButtonImage($source, $alt = null) {
-    if (!isset($alt))
-      $alt = $this->dictionary->text(util\Dictionary::NEXT_PAGE);
-    $this->nextPageButtonImage = util\HTML::image($source, $alt);
+  public function setNextPageButtonContent($content) {
+    $this->nextPageButtonContent = $content;
   }
 
   /**
-   * Image tag with image representing the last page
+   * 
    * @return string
    */
-  public function getLastPageButtonImage() {
-
-    if (isset($this->lastPageButtonImage)) {
-      return $this->lastPageButtonImage;
-    } else {
-
-      $source = LIB_ICONS_PATH . '/control_last.png';
-      $alt = $this->dictionary->text(util\Dictionary::LAST_PAGE);
-      return util\HTML::image($source, $alt, $this->getDefaultButtonStyle());
-    }
+  public function getLastPageButtonContent() {
+    return $this->lastPageButtonContent;
   }
 
   /**
-   * Image tag with image representing the last page
-   * @param string $source image location
-   * @param string $alt image alternative text
+   * 
+   * @param string $content
    * @return void
    */
-  public function setLastPageButtonImage($source, $alt = null) {
-    if (!isset($alt))
-      $alt = $this->dictionary->text(util\Dictionary::LAST_PAGE);
-    $this->lastPageButtonImage = util\HTML::image($source, $alt);
-  }
-
-  /**
-   * Additional Tag parameters
-   * @return string
-   */
-  public function getDefaultButtonStyle() {
-    return $this->defaultButtonStyle;
-  }
-
-  /**
-   * Additional Tag parameters
-   * @param type $defaultButtonStyle
-   * @return void
-   */
-  protected function setDefaultButtonStyle($defaultButtonStyle) {
-    $this->defaultButtonStyle = $defaultButtonStyle;
+  public function setLastPageButton($content) {
+    $this->lastPageButtonContent = $content;
   }
 
   /**
@@ -290,12 +181,9 @@ class RecordListViewer {
    * @return string HTML code
    */
   public function getFirstPageButton($title = null) {
-
-    if (!isset($title))
-      $title = $this->dictionary->text(util\Dictionary::FIRST_PAGE);
-
-    return util\HTML::link(
-                    $this->getPageLinkSource(1), $this->getFirstPageButtonImage(), $title);
+    return '<a href="' . $this->getPageLinkSource(1) . '"' .
+            ' title="' . $title . '">' . $this->getFirstPageButtonContent() .
+            '</a>';
   }
 
   /**
@@ -306,12 +194,10 @@ class RecordListViewer {
    * @return string HTML code
    */
   public function getPreviousPageButton($title = null) {
-
-    if (!isset($title))
-      $title = $this->dictionary->text(util\Dictionary::PREV_PAGE);
-
-    return util\HTML::link(
-                    $this->getPageLinkSource($this->recordList->getPreviousPage()), $this->getPreviousPageButtonImage(), $title);
+    $source = $this->getPageLinkSource($this->recordList->getPreviousPage());
+    return '<a href="' . $source . '"' .
+            ' title="' . $title . '">' . $this->getPreviousPageButtonContent() .
+            '</a>';
   }
 
   /**
@@ -322,28 +208,24 @@ class RecordListViewer {
    * @return string HTML code
    */
   public function getNextPageButton($title = null) {
-
-    if (!isset($title))
-      $title = $this->dictionary->text(util\Dictionary::NEXT_PAGE);
-
-    return util\HTML::link(
-                    $this->getPageLinkSource($this->recordList->getNextPage()), $this->getNextPageButtonImage(), $title);
+    $source = $this->getPageLinkSource($this->recordList->getNextPage());
+    return '<a href="' . $source . '"' .
+            ' title="' . $title . '">' . $this->getNextPageButtonContent() .
+            '</a>';
   }
 
   /**
    * HTML code for last page button
    * This method does not consider if it is already on the last page
-   * Consider calling getFarwardButtons() instead
+   * Consider calling getForwardButtons() instead
    * @param string $title link title parameter
    * @return string HTML code
    */
   public function getLastPageButton($title = null) {
-
-    if (!isset($title))
-      $title = $this->dictionary->text(util\Dictionary::LAST_PAGE);
-
-    return util\HTML::link(
-                    $this->getPageLinkSource($this->recordList->getTotalPages()), $this->getLastPageButtonImage(), $title);
+    $source = $this->getPageLinkSource($this->recordList->getTotalPages());
+    return '<a href="' . $source . '"' .
+            ' title="' . $title . '">' . $this->getLastPageButtonContent() .
+            '</a>';
   }
 
   /**
@@ -352,17 +234,15 @@ class RecordListViewer {
    * If page rewinding is not necessery, it will return inactive buttons
    */
   public function getRewindButtons() {
-
     if ($this->recordList->getTotalPages() > 1) {
-
-      if ($this->recordList->getCurrentPage() > 1 || $this->recordList->getCurrentPage() == $this->recordList->getTotalPages()) {
-
+      if ($this->recordList->getCurrentPage() > 1 ||
+              $this->recordList->getCurrentPage() ==
+              $this->recordList->getTotalPages()) {
         return $this->getFirstPageButton() . $this->getPreviousPageButton();
       } else {
-
         return '<span style="opacity: 0.5">' .
-                $this->getFirstPageButtonImage() .
-                $this->getPreviousPageButtonImage() .
+                $this->getFirstPageButton() .
+                $this->getPreviousPageButton() .
                 '</span>';
       }
     }
@@ -374,17 +254,15 @@ class RecordListViewer {
    * If page forwarding is not necessery, it will return inactive buttons
    */
   public function getForwardButtons() {
-
     if ($this->recordList->getTotalPages() > 1) {
-
-      if ($this->recordList->getCurrentPage() == 1 || $this->recordList->getCurrentPage() < $this->recordList->getTotalPages()) {
-
+      if ($this->recordList->getCurrentPage() == 1 ||
+              $this->recordList->getCurrentPage() <
+              $this->recordList->getTotalPages()) {
         return $this->getNextPageButton() . $this->getLastPageButton();
       } else {
-
         return '<span style="opacity: 0.5">' .
-                $this->getNextPageButtonImage() .
-                $this->getLastPageButtonImage() .
+                $this->getNextPageButton() .
+                $this->getLastPageButton() .
                 '</span>';
       }
     }
@@ -396,14 +274,18 @@ class RecordListViewer {
    * @return string HTML code
    */
   public function getControls($model = self::COMPLETE_CONTROLS) {
-
     switch ($model) {
-
       case self::COMPLETE_CONTROLS:
-        return $this->getRewindButtons() . ' ' . $this->recordList->getCurrentPage() . ' - ' . $this->recordList->getTotalPages() . ' ' . $this->getForwardButtons();
+        return $this->getRewindButtons() . ' ' .
+                $this->recordList->getCurrentPage() . ' - ' .
+                $this->recordList->getTotalPages() . ' ' .
+                $this->getForwardButtons();
         break;
       case self::SIMPLE_CONTROLS:
-        return $this->recordList->getCurrentPage() . ' - ' . $this->recordList->getTotalPages() . ' ' . $this->getPreviousPageButton() . $this->getNextPageButton();
+        return $this->recordList->getCurrentPage() . ' - ' .
+                $this->recordList->getTotalPages() . ' ' .
+                $this->getPreviousPageButton() .
+                $this->getNextPageButton();
         break;
     }
   }
@@ -416,14 +298,10 @@ class RecordListViewer {
    * @param type $className HTML class parameter value
    * @return string
    */
-  public function getTable($columnsHeaders = null, $id = "", $className = "") {
-
-
-
-    $table = "<table id=\"$id\" class=\"$className\" >";
+  public function getTable($columnsHeaders = null, $tagParameters) {
+    $table = "<table $tagParameters >";
     $table .= "<thead>";
     $table .= "<tr>";
-
     if (isset($columnsHeaders)) {
       $columnsHeadersArray = explode(",", $columnsHeaders);
       foreach ($columnsHeadersArray as $header) {
@@ -434,25 +312,18 @@ class RecordListViewer {
         $table .= "<th>$header->name</th>";
       }
     }
-
     $table .= "</tr>";
     $table .= "</thead>";
     $table .= "<tbody>";
-
     while ($rs = $this->getRecordList()->getSqlResult()->fetch_row()) {
-
       $table .= "<tr>";
-
       for ($i = 0; $i < count($rs); $i++) {
         $table .= "<td>$rs[$i]</td>";
       }
-
       $table .= "</tr>";
     }
-
     $table .= "</tbody>";
     $table .= '</table>';
-
     return $table;
   }
 
