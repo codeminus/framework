647aede11ae65876bbc96289b354cf99a0881170
Wilson Santos <wilson@codeminus.org>
2013-06-02 22:26:18 -0300

    org\codeminus\css\test.php improved

--- org/codeminus/css/test.php
+++ org/codeminus/css/test.php
@@ -3,21 +3,150 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
     <title></title>
-    <link rel="stylesheet" href="codeminus.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
+    <link rel="stylesheet" href="base.css" />
+    <link rel="stylesheet" href="containers.css" />
+    <link rel="stylesheet" href="navs.css" />
+    <link rel="stylesheet" href="forms.css" />
   </head>
   <body>
-    <div class="header-container">
-      <img src="../img/cmf-medium.png" />
+    <div class="container-header">
+      <div class="container-centered">
+        <img src="../img/cmf-medium.png" />
+      </div>
     </div>
-    <div class="container">
-      <ul class="nav">
+    <!--Headers
+    <h1>Header</h1>
+    <h2>Header</h2>
+    <h3>Header</h3>
+    <h4>Header</h4>
+    <h5>Header</h5>
+    <h6>Header</h6>
+    -->
+    <div class="container-centered">
+      <ul class="nav nav-hlist">
         <li><a href="#">Menu Item</a></li>
+        <li><a href="#" class="selected">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+      </ul>
+      <div class="separator"></div>
+      <h3 class="">Header</h3>
+      <!--horizontal tabs
+      <ul class="nav nav-htabs block">
+        <li><a href="#" class="selected">Menu Item</a></li>
         <li><a href="#">Menu Item</a></li>
         <li><a href="#">Menu Item</a></li>
         <li><a href="#">Menu Item</a></li>
       </ul>
+      -->
+      <!--vertical tabs
+      <ul class="nav nav-vtabs">
+        <li><a href="#" class="selected">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+      </ul>
+      -->
+      <!--vertical right side tabs
+      <ul class="nav nav-vtabs inverted">
+        <li><a href="#" class="selected">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+        <li><a href="#">Menu Item</a></li>
+      </ul>
+      -->
+      <section class="container-box rounded">
+        <header>Header</header>
+        <section>Content</section>
+        <footer>Footer</footer>
+      </section>
+      <section class="container-box rounded" style="width: 200px">
+        <header>Header</header>
+        <section>Content</section>
+        <footer class="text-right">Footer right-aligned</footer>
+      </section>
+      <section class="container-box rounded">
+        <section>Content</section>
+        <section>Content</section>
+        <section>Content</section>
+      </section>
+      <section class="container-box rounded">
+        <header>Header</header>
+        <section>Content</section>
+      </section>
+      <section class="container-box rounded">
+        <header>Header</header>
+        <footer>Footer</footer>
+      </section>
+      <div class="container-bubble">
+        <section class="container-box" style="width: 200px">
+          <header>container-bubble</header>
+          <section>Content</section>
+          <footer>Footer</footer>
+        </section>
+      </div>
+      <div class="container-shadow">
+        <section class="container-box" style="width: 200px">
+          <header>container-shadow</header>
+          <section>Content</section>
+          <footer>Footer</footer>
+        </section>
+      </div>
+      <div class="container-shadow">
+        <section class="container-box" style="width: 200px">
+          <section>Content</section>
+        </section>
+      </div>
+      <!--
+      <div class="container-foot">
+        <ul class="nav nav-simple">
+          <li class="header">Codeminus</li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Join us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <ul class="nav nav-simple">
+          <li class="header">Products</li>
+          <li><a href="#">Admos</a></li>
+          <li><a href="#">Codeminus Framework</a></li>
+          <li><a href="#">Odel</a></li>
+          <li><a href="#">WhoWorks</a></li>
+          <li><a href="#">Domain Registration</a></li>
+          <li><a href="#">Domain Hosting</a></li>
+        </ul>
+        <ul class="nav nav-simple">
+          <li class="header">Menu header</li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+        <ul class="nav nav-simple">
+          <li class="header">Menu header</li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+        <ul class="nav nav-simple">
+          <li class="header">Menu header</li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+      </div>
+      -->
+      <div class="container-foot clearfix">
+        <!--<ul class="nav nav-simple inline">
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>-->
+        <span class="float-right">&copy; 2013 Codeminus. All rights reserved</span>
+      </div>
     </div>
-
-
   </body>
 </html>
