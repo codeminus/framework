2f30971bb99f35bd195a95c454396fb02218d375
Wilson Santos <wilson@codeminus.org>
2013-06-03 19:28:14 -0300

    org\codeminus\css improved

--- org/codeminus/css/index.php
+++ org/codeminus/css/index.php
@@ -1,8 +1,22 @@
+<?php
+$contents = array(
+    'base' => false,
+    'containers' => false,
+    'navs' => false,
+    'forms' => false,
+    'tables' => false,
+);
+if (isset($_GET['c'])) {
+  $contents[$_GET['c']] = 'selected';
+} else {
+  $contents['base'] = 'selected';
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <title></title>
+    <title>Codeminus CSS Library</title>
     <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
     <link rel="stylesheet" href="base.css" />
     <link rel="stylesheet" href="containers.css" />
@@ -15,47 +29,52 @@
         <img src="../img/cmf-medium.png" />
       </div>
     </div>
-    <!--Headers
-    <h1>Header</h1>
-    <h2>Header</h2>
-    <h3>Header</h3>
-    <h4>Header</h4>
-    <h5>Header</h5>
-    <h6>Header</h6>
-    -->
     <div class="container-centered">
       <ul class="nav nav-hlist">
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#" class="selected">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
+        <li><a href="?c=base" class="<?php echo $contents['base'] ?>">Base</a></li>
+        <li><a href="?c=containers" class="<?php echo $contents['containers'] ?>">Containers</a></li>
+        <li><a href="?c=navs" class="<?php echo $contents['navs'] ?>">Navigations</a></li>
+        <li><a href="?c=tables" class="<?php echo $contents['forms'] ?>">Forms</a></li>
+        <li><a href="?c=tables" class="<?php echo $contents['tables'] ?>">Tables</a></li>
+        <li><a href="?c=tables" class="<?php echo $contents['media'] ?>">Media</a></li>
       </ul>
       <div class="separator"></div>
-      <h3 class="">Header</h3>
-      <!--horizontal tabs
-      <ul class="nav nav-htabs block">
-        <li><a href="#" class="selected">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-      </ul>
-      -->
-      <!--vertical tabs
-      <ul class="nav nav-vtabs">
-        <li><a href="#" class="selected">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-      </ul>
-      -->
-      <!--vertical right side tabs
-      <ul class="nav nav-vtabs inverted">
-        <li><a href="#" class="selected">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-        <li><a href="#">Menu Item</a></li>
-      </ul>
-      -->
+      <?php if ($contents['base']) { ?>
+        <h1>Header</h1>
+        <h2>Header</h2>
+        <h3>Header</h3>
+        <h4>Header</h4>
+        <h5>Header</h5>
+        <h6>Header</h6>
+      <?php } ?>
+      <?php if ($contents['navs']) { ?>
+        <ul class="nav nav-htabs block">
+          <li><a href="#" class="selected">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+        <ul class="nav nav-htabs block inverted">
+          <li><a href="#" class="selected">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+        <ul class="nav nav-vtabs ">
+          <li><a href="#" class="selected">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+
+        <ul class="nav nav-vtabs inverted">
+          <li><a href="#" class="selected">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Menu Item</a></li>
+        </ul>
+      <?php } ?>
+      <?php if ($contents['containers']) { ?>
       <section class="container-box rounded">
         <header>Header</header>
         <section>Content</section>
@@ -98,8 +117,9 @@
           <section>Content</section>
         </section>
       </div>
-      <!--
-      <div class="container-foot">
+      <?php } ?>
+      
+      <div class="container-upper-foot">
         <ul class="nav nav-simple">
           <li class="header">Codeminus</li>
           <li><a href="#">About us</a></li>
@@ -115,36 +135,14 @@
           <li><a href="#">Domain Registration</a></li>
           <li><a href="#">Domain Hosting</a></li>
         </ul>
-        <ul class="nav nav-simple">
-          <li class="header">Menu header</li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-        <ul class="nav nav-simple">
-          <li class="header">Menu header</li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-        <ul class="nav nav-simple">
-          <li class="header">Menu header</li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
       </div>
-      -->
-      <div class="container-foot clearfix">
-        <!--<ul class="nav nav-simple inline">
+      <div class="container-lower-foot clearfix">
+        <ul class="nav nav-simple inline">
           <li><a href="#">Menu Item</a></li>
           <li><a href="#">Menu Item</a></li>
           <li><a href="#">Menu Item</a></li>
           <li><a href="#">Menu Item</a></li>
-        </ul>-->
+        </ul>
         <span class="float-right">&copy; 2013 Codeminus. All rights reserved</span>
       </div>
     </div>
