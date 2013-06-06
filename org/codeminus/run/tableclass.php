3e27b5d68a2ae8f6bdfff2a50d7432a35b731758
Wilson Santos <wilson@codeminus.org>
2013-06-06 17:50:08 -0300

    org\codmeinus\css\ improved

--- org/codeminus/run/tableclass.php
+++ org/codeminus/run/tableclass.php
@@ -25,14 +25,10 @@ if (isset($_POST['cmd'])) {
 <html>
   <head>
     <title>Table class generator</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
     <link rel="stylesheet" href="../css/base.css" />
     <link rel="stylesheet" href="../css/containers.css" />
     <link rel="stylesheet" href="../css/forms.css" />
-    <style type="text/css">
-      .container-box{
-        margin-bottom: 20px;
-      }
-    </style>
   </head>
   <body>
     <div class="container-header">
@@ -43,39 +39,46 @@ if (isset($_POST['cmd'])) {
     </div>
     <div class="container-centered">
       <section><h5>Table class generator</h5></section>
-      <div class="float-left" >
+      <div class="float-left margined-right">
         <form name="" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-          <section class="container-box rounded block">
+          <section class="container-box rounded block margined-bottom">
             <header>Database connection parameters</header>
-            <section>
-              <label for="db_host">Host:</label>
-              <input type="text" name="db_host" id="db_host" value="<?php echo $db_host ?>" class="medium"/>
-              <label for="db_user">User:</label>
-              <input type="text" name="db_user" id="db_user" value="<?php echo $db_user ?>" class="medium"/>
-              <label for="db_pass">Password:</label>
-              <input type="text" name="db_pass" id="db_pass" value="<?php echo $db_pass ?>" class="medium"/>
-              <label for="db_name">Database:</label>
-              <input type="text" name="db_name" id="db_name" value="<?php echo $db_name ?>" class="medium"/>
-              <label for="db_table">Table name:</label>
-              <input type="text" name="db_table" id="db_table" value="<?php echo $db_table ?>" class="medium"/>
+            <section class="text-right">
+              <label for="db_host">Host</label>
+              <input type="text" name="db_host" id="db_host" 
+                     value="<?php echo $db_host ?>" /><br/>
+              <label for="db_user">User</label>
+              <input type="text" name="db_user" id="db_user" 
+                     value="<?php echo $db_user ?>" /><br/>
+              <label for="db_pass">Password</label>
+              <input type="text" name="db_pass" id="db_pass" 
+                     value="<?php echo $db_pass ?>" /><br/>
+              <label for="db_name">Database</label>
+              <input type="text" name="db_name" id="db_name" 
+                     value="<?php echo $db_name ?>" /><br/>
+              <label for="db_table">Table name</label>
+              <input type="text" name="db_table" id="db_table" 
+                     value="<?php echo $db_table ?>"/>
             </section>
-
           </section>
           <section class="container-box rounded block">
             <header>Class properties</header>
             <section>
-              <label for="class_namespace">Namespace:</label>
-              app\models\ <input type="text" name="class_namespace" id="class_namespace" value="<?php echo $class_namespace ?>" class=""/>
+              <label for="class_namespace">Namespace</label><br/>
+              <span class="input-group">
+                <span>app\models\</span>
+                <input type="text" name="class_namespace" 
+                       id="class_namespace" value="<?php echo $class_namespace ?>" />
+              </span>
             </section>
           </section>
           <input type="submit" name="cmd" value="generate" class="btn btn-blue"/>
         </form>
       </div>
-      <div class="float-right" style="width: 630px;">
+      <div class="float-left" style="width: 66.70%">
         <section class="container-box rounded block">
           <header>Output</header>
           <section style="height: 492px; overflow-y: auto;">
-
             <?php
             if (isset($_POST['cmd'])) {
               $dbconn = new db\Connection($_POST['db_host'], $_POST['db_user'], $_POST['db_pass'], $_POST['db_name']);
