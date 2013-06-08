fb409dae5b53f1de17f3f3ffe458714bd48959ca
Wilson Santos <wilson@codeminus.org>
2013-06-08 19:30:21 -0300

    added jquery and codeminus.js importation

--- org/codeminus/app-skeleton/app/views/shared/footer.php
+++ org/codeminus/app-skeleton/app/views/shared/footer.php
@@ -1,2 +1,4 @@
+    <script type="text/javascript" src="<?php echo APP_HTTP_PATH ?>/org/codeminus/js/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo APP_HTTP_PATH ?>/org/codeminus/js/codeminus.js"></script>  
   </body>
 </html>
