a29672bd068f59e6aa115be4b50341674114e105
Wilson Santos <wilson@codeminus.org>
2013-06-21 14:36:15 -0300

    org\codeminus\js data-tab handler implemented

--- org/codeminus/css/doc/navs.php
+++ org/codeminus/css/doc/navs.php
@@ -4,28 +4,27 @@
       <li class="header">.nav-hlist</li>
       <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
-      <li><a href="#">Menu Item</a></li>
       <li class="dropdown">
-        <a href="#" class="trigger">
+        <a class="trigger">
           Dropdown <span class="caret"></span>
         </a>
-        <ul class="dropdown-menu">
+        <ul class="drop-menu">
           <li><a href="#">Menu Item</a></li>
           <li><a href="#" class="disabled">Menu Item</a></li>
           <li><a href="#">Menu Item</a></li>
           <li class="divider"></li>
           <li class="submenu">
-            <a href="#" class="trigger">
+            <a class="trigger">
               Menu Item <span class="caret"></span>
             </a>
-            <ul class="dropdown-menu">
+            <ul class="drop-menu">
               <li><a href="#">Menu Item</a></li>
               <li><a href="#">Menu Item</a></li>
               <li class="submenu">
-                <a href="#" class="trigger">
+                <a class="trigger">
                   Menu Item <span class="caret"></span>
                 </a>
-                <ul class="dropdown-menu">
+                <ul class="drop-menu">
                   <li><a href="#">Menu Item</a></li>
                   <li><a href="#">Menu Item</a></li>
                   <li><a href="#">Menu Item</a></li>
@@ -43,13 +42,11 @@
       <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
-      <li><a href="#">Menu Item</a></li>
     </ul>
   </div><br/>
   <div class="container-bubble">
     <ul class="nav nav-simple">
       <li class="header">.nav-simple</li>
-      <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
       <li><a href="#">Menu Item</a></li>
@@ -79,81 +76,189 @@
       <li class="footer">.footer</li>
     </ul>
   </div>
-  <ul class="nav nav-vlist style02">
-    <li class="header">.nav-vlist .style02</li>
-    <li><a href="#" >Menu Item</a></li>
-    <li><a href="#" class="active">Menu Item</a></li>
-    <li><a href="#">Menu Item</a></li>
-    <li><a href="#">Menu Item</a></li>
-    <li class="footer">.footer</li>
-  </ul>
-  <div class="container-bubble ">
-    <section class="container-box block">
-      <section>
-        <ul class="nav nav-vtabs">
-          <li class="header">.nav-vtabs</li>
-          <li><a href="#" class="active">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-        <span>Content</span>
-      </section>
+  <br/>
+  <section class="container-box container-bubble">
+    <section>
+      <ul class="nav nav-vtabs tab-triggers span1">
+        <li class="header">.nav-vtabs</li>
+        <li><a data-tab-target="tab1" class="active">Menu Item</a></li>
+        <li><a data-tab-target="tab2">Menu Item</a></li>
+        <li><a data-tab-target="tab3">Menu Item</a></li>
+      </ul>
+      <div class="span3 margined-left">
+        <section class="tab active" id="tab1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus quis lectus metus, at posuere neque.
+          Sed pharetra nibh eget orci convallis at posuere leo convallis.
+          Sed blandit augue vitae augue scelerisque bibendum.
+          Vivamus sit amet libero turpis, non venenatis urna.
+          In blandit, odio convallis suscipit venenatis,
+          ante ipsum cursus augue.
+        </section>
+        <section class="tab" id="tab2">
+          Et mollis nunc diam eget sapien. Nulla facilisi.
+          Etiam feugiat imperdiet rhoncus.
+          Sed suscipit bibendum enim, sed volutpat tortor malesuada non.
+          Morbi fringilla dui non purus porttitor mattis.
+          Suspendisse quis vulputate risus.
+          Phasellus erat velit, sagittis sed varius volutpat, placerat nec urna.
+          Nam eu metus vitae dolor fringilla feugiat. Nulla.
+        </section>
+        <section class="tab" id="tab3">
+          Facilisi. Etiam enim metus, luctus in adipiscing at,
+          consectetur quis sapien. Duis imperdiet egestas ligula,
+          quis hendrerit ipsum ullamcorper et. Phasellus id tristique orci.
+          Proin consequat mi at felis scelerisque ullamcorper.
+          Etiam tempus, felis vel eleifend porta, velit nunc mattis urna,
+          at ullamcorper erat diam dignissim ante. Pellentesque justo risus.
+        </section>
+      </div>
     </section>
-  </div>
-  <div class="container-bubble ">
-    <section class="container-box block">
-      <section>
-        <span>content</span>
-        <ul class="nav nav-vtabs inverted">
-          <li class="header">.nav-vtabs .inverted</li>
-          <li><a href="#" class="active">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-      </section>
+  </section>
+  <section class="container-box container-bubble">
+    <section>
+      <div class="span3 margined-right">
+        <section class="tab" id="tab11">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus quis lectus metus, at posuere neque.
+          Sed pharetra nibh eget orci convallis at posuere leo convallis.
+          Sed blandit augue vitae augue scelerisque bibendum.
+          Vivamus sit amet libero turpis, non venenatis urna.
+          In blandit, odio convallis suscipit venenatis,
+          ante ipsum cursus augue.
+        </section>
+        <section class="tab active" id="tab22">
+          Et mollis nunc diam eget sapien. Nulla facilisi.
+          Etiam feugiat imperdiet rhoncus.
+          Sed suscipit bibendum enim, sed volutpat tortor malesuada non.
+          Morbi fringilla dui non purus porttitor mattis.
+          Suspendisse quis vulputate risus.
+          Phasellus erat velit, sagittis sed varius volutpat, placerat nec urna.
+          Nam eu metus vitae dolor fringilla feugiat. Nulla.
+        </section>
+        <section class="tab" id="tab33">
+          Facilisi. Etiam enim metus, luctus in adipiscing at,
+          consectetur quis sapien. Duis imperdiet egestas ligula,
+          quis hendrerit ipsum ullamcorper et. Phasellus id tristique orci.
+          Proin consequat mi at felis scelerisque ullamcorper.
+          Etiam tempus, felis vel eleifend porta, velit nunc mattis urna,
+          at ullamcorper erat diam dignissim ante. Pellentesque justo risus.
+        </section>
+      </div>
+      <ul class="nav nav-vtabs inverted tab-triggers span1">
+        <li class="header">.nav-vtabs .inverted</li>
+        <li><a data-tab-target="tab11">Menu Item</a></li>
+        <li><a data-tab-target="tab22" class="active">Menu Item</a></li>
+        <li><a data-tab-target="tab33">Menu Item</a></li>
+      </ul>
     </section>
-  </div>
-  <div class="container-bubble">
-    <section class="container-box">
-      <section>
-        <ul class="nav nav-hlist">
-          <li class="header">.nav-hlist</li>
-          <li><a href="#" class="active">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-      </section>
+  </section>
+  <section class="container-box container-bubble">
+    <section class="span4">
+      <ul class="nav nav-htabs block tab-triggers">
+        <li><a data-tab-target="tab111" class="active">Menu Item 01</a></li>
+        <li><a data-tab-target="tab222">Menu Item 02</a></li>
+        <li class="dropdown">
+          <a class="trigger tab-trigger">
+            Dropdown
+            <span class="caret"></span>
+          </a>
+          <ul class="drop-menu">
+            <li><a data-tab-target="tab333">Menu Item 03</a></li>
+            <li><a data-tab-target="tab444">Menu Item 04</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div>
+        <section class="tab active" id="tab111">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus quis lectus metus, at posuere neque.
+          Sed pharetra nibh eget orci convallis at posuere leo convallis.
+          Sed blandit augue vitae augue scelerisque bibendum.
+          Vivamus sit amet libero turpis, non venenatis urna.
+          In blandit, odio convallis suscipit venenatis,
+          ante ipsum cursus augue.
+        </section>
+        <section class="tab" id="tab222">
+          Et mollis nunc diam eget sapien. Nulla facilisi.
+          Etiam feugiat imperdiet rhoncus.
+          Sed suscipit bibendum enim, sed volutpat tortor malesuada non.
+          Morbi fringilla dui non purus porttitor mattis.
+          Suspendisse quis vulputate risus.
+          Phasellus erat velit, sagittis sed varius volutpat, placerat nec urna.
+          Nam eu metus vitae dolor fringilla feugiat. Nulla.
+        </section>
+        <section class="tab" id="tab333">
+          Facilisi. Etiam enim metus, luctus in adipiscing at,
+          consectetur quis sapien. Duis imperdiet egestas ligula,
+          quis hendrerit ipsum ullamcorper et. Phasellus id tristique orci.
+          Proin consequat mi at felis scelerisque ullamcorper.
+          Etiam tempus, felis vel eleifend porta, velit nunc mattis urna,
+          at ullamcorper erat diam dignissim ante. Pellentesque justo risus.
+        </section>
+        <section class="tab" id="tab444">
+          Rutrum ac semper a, faucibus nec lorem. Nullam eget quam tellus,
+          eget sagittis justo.Class aptent taciti sociosqu ad litora torquent
+          per conubia nostra, per inceptos himenaeos. Proin ante enim,
+          tincidunt ut interdum in, adipiscing quis tortor. Nulla turpis lacus,
+          rutrum in adipiscing ut, porttitor ac ante. Sed euismod, mauris a.
+        </section>
+      </div>
     </section>
-  </div>
-  <div class="container-bubble">
-    <section class="container-box">
-      <section>
-        <ul class="nav nav-htabs block">
-          <li><a href="#" class="active">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-        <span>.nav-htabs</span>
-      </section>
+  </section>
+  <section class="container-box container-bubble">
+    <section class="span4">
+      <div>
+        <section class="tab active" id="tab1111">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus quis lectus metus, at posuere neque.
+          Sed pharetra nibh eget orci convallis at posuere leo convallis.
+          Sed blandit augue vitae augue scelerisque bibendum.
+          Vivamus sit amet libero turpis, non venenatis urna.
+          In blandit, odio convallis suscipit venenatis,
+          ante ipsum cursus augue.
+        </section>
+        <section class="tab" id="tab2222">
+          Et mollis nunc diam eget sapien. Nulla facilisi.
+          Etiam feugiat imperdiet rhoncus.
+          Sed suscipit bibendum enim, sed volutpat tortor malesuada non.
+          Morbi fringilla dui non purus porttitor mattis.
+          Suspendisse quis vulputate risus.
+          Phasellus erat velit, sagittis sed varius volutpat, placerat nec urna.
+          Nam eu metus vitae dolor fringilla feugiat. Nulla.
+        </section>
+        <section class="tab" id="tab3333">
+          Facilisi. Etiam enim metus, luctus in adipiscing at,
+          consectetur quis sapien. Duis imperdiet egestas ligula,
+          quis hendrerit ipsum ullamcorper et. Phasellus id tristique orci.
+          Proin consequat mi at felis scelerisque ullamcorper.
+          Etiam tempus, felis vel eleifend porta, velit nunc mattis urna,
+          at ullamcorper erat diam dignissim ante. Pellentesque justo risus.
+        </section>
+        <section class="tab" id="tab4444">
+          Rutrum ac semper a, faucibus nec lorem. Nullam eget quam tellus,
+          eget sagittis justo.Class aptent taciti sociosqu ad litora torquent
+          per conubia nostra, per inceptos himenaeos. Proin ante enim,
+          tincidunt ut interdum in, adipiscing quis tortor. Nulla turpis lacus,
+          rutrum in adipiscing ut, porttitor ac ante. Sed euismod, mauris a.
+        </section>
+      </div>
+      <ul class="nav nav-htabs inverted block tab-triggers ">
+        <li><a data-tab-target="tab1111" class="active">Menu Item 01</a></li>
+        <li><a data-tab-target="tab2222">Menu Item 02</a></li>
+        <li class="dropup">
+          <a class="trigger tab-trigger">
+            Dropup
+            <span class="caret"></span>
+          </a>
+          <ul class="drop-menu">
+            <li><a data-tab-target="tab3333">Menu Item 03</a></li>
+            <li><a data-tab-target="tab4444">Menu Item 04</a></li>
+          </ul>
+        </li>
+      </ul>
     </section>
-  </div>
-  <div class="container-bubble">
-    <section class="container-box">
-      <section>
-        <span>.nav-htabs .inverted</span>
-        <ul class="nav nav-htabs inverted block">
-          <li><a href="#" class="active">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-          <li><a href="#">Menu Item</a></li>
-        </ul>
-      </section>
-    </section>
-  </div><br/>
+  </section><br/>
   <ul class="nav nav-pager">
     <li><a href="#" class="disabled"><span class="glyphicon-thin-arrow-left disabled"></span></a></li>
     <li><a href="#" class="active">1</a></li>
