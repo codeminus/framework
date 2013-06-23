e0106ca45630d9b7b9e072364063855a0d89b47d
Wilson Santos <wilson@codeminus.org>
2013-06-23 11:29:31 -0300

    org\codeminus\css\layouts.css implemened

---
+++ org/codeminus/css/doc/layouts.php
@@ -0,0 +1,28 @@
+<div class="row">
+  <div class="span3">.span3</div>
+  <div class="span11">.span11</div>
+  <div class="span1">.span1</div>
+</div>
+<div class="row">
+  <div class="span3">
+    <ul class="nav nav-vlist block">
+      <li><a href="#" class="active">Menu Item</a></li>
+      <li><a href="#">Menu Item</a></li>
+      <li><a href="#">Menu Item</a></li>
+    </ul>
+  </div>
+  <div class="span11">
+    <div class="container-box block">
+      <header>Header</header>
+      <section>Content</section>
+      <footer>Footer</footer>
+    </div>
+  </div>
+  <div class="span1">
+    <ul class="nav nav-vlist style02 block text-center">
+      <li><a href="#"><span class="glyphicon-home"></span></a></li>
+      <li><a href="#"><span class="glyphicon-user"></span></a></li>
+      <li><a href="#"><span class="glyphicon-calendar"></span></a></li>
+    </ul>
+  </div>
+</div>
