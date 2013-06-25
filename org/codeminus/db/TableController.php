ba2f23630979be0b102ff7df57fe505a2dc3e9fc
Wilson Santos <wilson@codeminus.org>
2013-06-25 09:18:55 -0300

    org\codeminus\db TableController interface implemented

---
+++ org/codeminus/db/TableController.php
@@ -0,0 +1,17 @@
+<?php
+namespace org\codeminus\db;
+/**
+ * TableController Interface
+ * Defines methods for database table controllers
+ * @author Wilson Santos <web@wilsonssantos.com>
+ * @version 1.0
+ */
+interface TableController {
+  //Views
+  public function add();
+  public function edit($id);
+  //Actions
+  public function insertAction();
+  public function updateAction($id);
+  public function deleteAction($id);
+}
