60b3728a2ce44d8ef542e5d72d7969fc36ecc779
Wilson Santos <wilson@codeminus.org>
2013-06-27 12:27:20 -0300

    org\codeminus\file\FileHandler.php version 1.1 implemented. New methods for file extension handling implemented

--- org/codeminus/file/FileHandler.php
+++ org/codeminus/file/FileHandler.php
@@ -2,35 +2,29 @@
 
 namespace org\codeminus\file;
 
+use org\codeminus\util as util;
+
 /**
  * FileHandler
  *
  * @author Wilson Santos <wilson@codeminus.org>
- * @version 1.0
+ * @version 1.1
  */
 class FileHandler {
-
+  
   /**
    * Copies all files from a folder to another recursively
    * @param string $src source path to copy from
    * @param string $dst destination path to copy to
-   * @param boolean $showLog if true it will output messages about what is happening inside the method.
    * @return void
    */
-  public static function recursiveCopy($src, $dst, $showLog = true, $groupLog = true) {
-    if ($groupLog) {
-      echo '<p class="info">_</p>';
-    }
+  public static function recursiveCopy($src, $dst) {
     $dir = opendir($src);
     if (!file_exists($dst)) {
       mkdir($dst);
-      if ($showLog) {
-        echo '<p class="info">' . $dst . ' created.</p>';
-      }
+      util\ClassLog::add(__METHOD__, $dst . ' created');
     } else {
-      if ($showLog) {
-        echo '<p class="warning">' . $dst . ' NOT created. Directory already exists.</p>';
-      }
+      util\ClassLog::add(__METHOD__, $dst . ' not created. Directory already exists', util\ClassLog::LOG_WARNNING);
     }
     while (false !== ( $file = readdir($dir))) {
       if (( $file != '.' ) && ( $file != '..' )) {
@@ -41,13 +35,9 @@ class FileHandler {
         } else {
           if (!file_exists($destinationFilePath)) {
             copy($sourceFilePath, $destinationFilePath);
-            if ($showLog) {
-              echo '<p class="info">' . $destinationFilePath . ' created.</p>';
-            }
+            util\ClassLog::add(__METHOD__, $destinationFilePath . ' created');
           } else {
-            if ($showLog) {
-              echo '<p class="warning">' . $destinationFilePath . ' NOT created. File already exists.</p>';
-            }
+            util\ClassLog::add(__METHOD__, $destinationFilePath . ' not created. File already exists', util\ClassLog::LOG_WARNNING);
           }
         }
       }
@@ -59,46 +49,65 @@ class FileHandler {
    * Creates a file
    * @param string $fileName complete file path
    * @param string $fileContent Content to be put inside the file
+   * method
    * @return void
    */
-  public static function createFile($fileName, $fileContent, $showLog = true) {
+  public static function createFile($fileName, $fileContent) {
     if (!file_exists($fileName)) {
       if (!file_put_contents($fileName, $fileContent)) {
         exit('<p>Unable to create ' . $fileName . '</p>');
       } else {
-        if ($showLog) {
-          echo '<p class="info">' . $fileName . ' created.</p>';
-        }
+        util\ClassLog::add(__METHOD__, $fileName . ' created');
       }
     } else {
-      if ($showLog) {
-        echo '<p class="warning">' . $fileName . ' NOT created. File already exists.</p>';
-      }
+      util\ClassLog::add(__METHOD__, $fileName . ' not created. File already exists', util\ClassLog::LOG_WARNNING);
     }
   }
 
   /**
    * Creates a directory
    * @param string $dir path and name
-   * @param octal $mode according to the access level defined by chmod
+   * @param octal $mode[optional] the access level desired for the folder
    * @return void;
    */
-  public static function createDir($dir, $mode = 0777, $showLog = true) {
+  public static function createDir($dir, $mode = 0755) {
     if (!file_exists($dir)) {
       if (!mkdir($dir, $mode, true)) {
         exit('<p class="warning">Unable to create ' . $dir . '</p>');
       } else {
-        if ($showLog) {
-          echo '<p class="info">' . $dir . ' created.</p>';
-        }
+        util\ClassLog::add(__METHOD__, $dir . ' created');
       }
     } else {
-      if ($showLog) {
-        echo '<p class="warning">' . $dir . ' NOT created. Directory already exists.</p>';
-      }
+      util\ClassLog::add(__METHOD__, $dir . ' not created. Directory already exists', util\ClassLog::LOG_WARNNING);
     }
   }
+  
+  /**
+   * File extension
+   * @param string $fileName Containing a filename
+   * @return string extension
+   */
+  public static function getFileExtension($fileName) {
+    $parts = explode(".", $fileName);
+    return strtolower(end($parts));
+  }
 
+  /**
+   * Validate file extension
+   * @param string $filename Containing a filename
+   * @param string $validExtensions List of extensions separated by ,(comma).
+   * <br/>Example: "gif, jpg, png"
+   * @return boolean TRUE if the extension is valid and FALSE otherwise.
+   */
+  public static function validateExtension($filename, $validExtensions) {
+    $fileExtension = self::getFileExtension($filename);
+    $validExtensions = explode(',', $validExtensions);
+    foreach ($validExtensions as $ext){
+      if($fileExtension == trim($ext)){
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
-
-?>
