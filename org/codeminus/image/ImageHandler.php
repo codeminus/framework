53813f6bd30dacb1312fab23e3e28c624f5238b7
Wilson Santos <wilson@codeminus.org>
2013-06-28 18:20:51 -0300

    ExtentedException 1.0 released

--- org/codeminus/image/ImageHandler.php
+++ org/codeminus/image/ImageHandler.php
@@ -31,7 +31,7 @@ class ImageHandler {
     $this->setSource($imageSource);
     $this->setImageInfo(getimagesize($imageSource));
     if(!$this->getImageInfo()){
-      throw new main\ExtException('<b>Error:</b> Unable to handle ' . $imageSource);
+      throw new main\ExtendedException('Unable to handle ' . $imageSource, main\ExtendedException::E_ERROR);
       return false;
     }
     switch ($this->getType()) {
