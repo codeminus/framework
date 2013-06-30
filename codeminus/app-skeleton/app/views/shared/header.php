b2caa0c40d72a16e3fb0c8f367f9376376aec9f4
Wilson Santos <wilson@codeminus.org>
2013-06-30 09:22:28 -0300

    CMF_ASSETS changed to CMF_ASSETS_PATH

--- codeminus/app-skeleton/app/views/shared/header.php
+++ codeminus/app-skeleton/app/views/shared/header.php
@@ -7,7 +7,7 @@ use \codeminus\main as main;
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <title><?php echo main\View::getTitle() ?></title>
         <link rel="shortcut icon" type="image/x-icon" 
-              href="<?php echo CMF_ASSETS ?>/img/favicon.ico">
+              href="<?php echo CMF_ASSETS_PATH ?>/img/favicon.ico">
         <link rel="stylesheet" href="<?php echo CMF_CSS_PATH ?>/codeminus.css"/>
     </head>
     <body>
