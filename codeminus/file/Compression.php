c8dceac056062f0ca5a9b5b5aea2388bdf21730f
Wilson Silva dos Santos Junior <cathaldallan@gmail.com>
2013-07-08 14:49:41 -0300

    zip method improved and extractZip method implemented

--- codeminus/file/Compression.php
+++ codeminus/file/Compression.php
@@ -2,26 +2,29 @@
 
 namespace codeminus\file;
 
+use codeminus\main as main;
+
 /**
  * Compression
  * @author Wilson Santos <wilson@codeminus.org>
  * @version 1.0
  */
 class Compression {
+
   private static $parentPath;
-  
-  private function __construct(){
+
+  private function __construct() {
     //prevent instantiation
   }
-  
+
   /**
    * Populates a zip file recursively
    * @param string $path the directory to add to the zip file
    * @param \ZipArchive $zip the zip file that will be populated
    * @return void
    */
-  private static function populateZip($path, &$zip) {    
-    if(!isset(self::$parentPath)){
+  private static function populateZip($path, \ZipArchive &$zip) {
+    if (!isset(self::$parentPath)) {
       self::$parentPath = dirname($path) . DIRECTORY_SEPARATOR;
     }
     $dirHandler = dir($path);
@@ -38,31 +41,63 @@ class Compression {
       }
     }
   }
-  
+
   /**
    * Creates a zip file
    * @param string $path The file of directory to add to the zip file
-   * @param string $destinationFile the path to the zip file
-   * @param boolean $overwrite If TRUE, it will overwrite the existent zip file
+   * @param string $destinationFile[optional] the path to the zip file
+   * @param boolean $overwrite[optional] If TRUE, it will overwrite the existent zip file
    * @return mixed Returns a string with the status message on success or false
    * on failure
+   * @throws main\ExtendedException
    */
-  public static function zip($path, $destinationFile, $overwrite = true) {
+  public static function zip($path, $destinationFile = null, $overwrite = true) {
+    if (!isset($destinationFile)) {
+      $destinationFile = $path . '.zip';
+    }
+
     $zip = new \ZipArchive();
     if ($overwrite) {
       $zip->open($destinationFile, \ZipArchive::OVERWRITE);
     } else {
       $zip->open($destinationFile, \ZipArchive::CREATE);
     }
-    if(is_dir($path)){
+
+    if (is_dir($path)) {
       $zip->addEmptyDir(basename($path));
       self::populateZip($path, $zip);
-    }else{
+    } else {
       $zip->addFile($path);
     }
     $status = $zip->getStatusString();
     $zip->close();
+    if (!is_file($destinationFile)) {
+      throw new main\ExtendedException($destinationFile . ' is not a file');
+    }
     return $status;
   }
-  
+
+  /**
+   * Extract a zip file to a given destination
+   * @param string $zipFile the zip file to be extracted
+   * @param string $destination[optional] Where to extract the zip file
+   * @return boolean TRUE if the extraction was successful and FALSE otherwise
+   * @throws main\ExtendedException
+   */
+  public static function extractZip($zipFile, $destination = null) {
+    if(!isset($destination)){
+      $destination = dirname($zipFile);
+    }
+    if (is_dir($destination)) {
+      $zip = new \ZipArchive();
+      if ($zip->open($zipFile)) {
+        return $zip->extractTo($destination);
+      } else {
+        throw new main\ExtendedException('Unable to open ' . $zipFile);
+      }
+    } else {
+      throw new main\ExtendedException($destination . ' is not a valid directory');
+    }
+  }
+
 }
