db065b1d1e28e4dba54e677893a035eb2bf2eebf
Wilson Silva dos Santos Junior <cathaldallan@gmail.com>
2013-07-08 15:20:04 -0300

    app index minor fix

--- codeminus/app-skeleton/index.php
+++ codeminus/app-skeleton/index.php
@@ -1,20 +1,2 @@
 <?php
 require_once ('app/configs/init.php');
-
-use \codeminus\db as db;
-
-class User extends db\Table{
-    
-    public function __construct(){
-        parent::__construct('user');
-    }
-    
-    public function insert(){}
-    public function update($where = null){
-        
-        $this->addUpdateField('name','um nome');
-        $this->createUpdateStatement($where);
-        
-    }
-    
-}
