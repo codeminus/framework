c75d1cafbbb8d5f0d41847683db391e769e4e5c7
Wilson Silva dos Santos Junior <cathaldallan@gmail.com>
2013-07-11 15:07:38 -0300

    app\controllers directory modified to app\controller. app\views directory modified to app\view

---
+++ codeminus/app-skeleton/app/controller/Error.php
@@ -0,0 +1,19 @@
+<?php
+use codeminus\main as main;
+
+##################################################
+# CHANGE THIS CONTROLLER ACCORDING TO YOUR NEEDS #
+##################################################
+
+class Error extends main\Controller{
+    
+    public function __construct() {
+        parent::__construct();
+        $this->view->setTitle('Error');
+    }
+    
+    public function index() {
+        $this->view->render('error/pageNotFound');
+    }
+
+}
