41d14608611ece87afdec6a28b3ed4a6cb1553c5
Wilson Santos <wilson@codeminus.org>
2013-07-18 21:58:49 -0300

    doc improved

--- codeminus/main/Autoloader.php
+++ codeminus/main/Autoloader.php
@@ -17,16 +17,29 @@ class Autoloader {
     //prevent clone
   }
 
+  /**
+   * Initializes the autoloader
+   * @return void
+   */
   public static function init() {
     spl_autoload_register();
     $cmfPath = preg_replace('/\\\/','/',substr(__FILE__, 0, strpos(__FILE__, '\codeminus')));
     self::includePath($cmfPath);
   }
 
+  /**
+   * The php include path
+   * @return array An array containing all include paths
+   */
   public static function getIncludePath(){
     return explode(PATH_SEPARATOR, get_include_path());
   }
   
+  /**
+   * Add paths to the php include path
+   * @param string $path
+   * @return void
+   */
   public static function includePath($path) {
     if(!array_search($path, self::getIncludePath())){
       set_include_path(get_include_path() . PATH_SEPARATOR . $path);
