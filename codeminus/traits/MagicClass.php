1fbe0aea74d5acf055257e506ebf21064c40d73b
Wilson Santos <wilson@codeminus.org>
2013-07-18 22:00:03 -0300

    traits\MagicClass implemented

---
+++ codeminus/traits/MagicClass.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace codeminus\traits;
+
+use codeminus\main as main;
+
+/**
+ * @author Wilson Santos <wilson@codeminus.org>
+ * @version 1.0
+ */
+trait MagicClass {
+
+  /**
+   * This magic method is trigged when invoking inaccessible methods
+   * @param string $method
+   * @param array $args
+   * @return mixed
+   * @throws main\ExtendedException
+   */
+  public function __call($method, $args) {
+    if (strpos($method, 'get') === 0) {
+      $property = str_replace('get', '', $method);
+      if (!property_exists($this, $property)) {
+        $property = lcfirst($property);
+        if (!property_exists($this, $property)) {
+          throw new main\ExtendedException("$property is not a " . __CLASS__
+          . " property", main\ExtendedException::E_ERROR);
+        }
+      }
+      return $this->$property;
+    } elseif (strpos($method, 'set') === 0) {
+      $property = str_replace('set', '', $method);
+      if (!property_exists($this, $property)) {
+        $property = lcfirst($property);
+        if (!property_exists($this, $property)) {
+          throw new main\ExtendedException("$property is not a " . __CLASS__
+          . " property", main\ExtendedException::E_ERROR);
+        }
+      }
+      $this->$property = $args[0];
+    }
+  }
+
+}
