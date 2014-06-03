44afe5d76ec360721ae2707970748aa7436cafb0
wilson <wilson@wilson-pc>
2014-06-03 14:23:27 -0300

    TableMessage implement to assist with database table system messages

--- codeminus/util/TableMessage.php
+++ codeminus/util/TableMessage.php
@@ -3,7 +3,7 @@
 namespace codeminus\util;
 
 /**
- * Table default system messages
+ * Default system messages for database table operation
  * @author Wilson Santos <wilson@codeminus.org>
  * @version 1.0
  */
@@ -11,11 +11,13 @@ class TableMessage extends SystemMessage {
   const REC_ADDED = -1;
   const REC_UPDATED = -2;
   const REC_DELETED = -3;
+  const NUll_FIELD = -4;
   
   public function __construct($code = self::NONE, $additionalInfo = "") {
     parent::__construct($code, $additionalInfo);
     $this->addMessage(self::REC_ADDED, 'Record added with success.');
     $this->addMessage(self::REC_UPDATED, 'Record updated with success.');
     $this->addMessage(self::REC_DELETED, 'Record deleted with success.');
+    $this->addMessage(self::NUll_FIELD, 'Required field not given.', SystemMessage::ERROR_MESSAGE);
   }
 }
