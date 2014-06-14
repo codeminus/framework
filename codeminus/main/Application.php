8abc2a898414813e44db35a686e577bd8e27b48a
Wilson <Wilson@Wilson-TOP>
2014-06-13 22:45:54 -0300

    APP_HTTP_PATH bug fixed

--- codeminus/main/Application.php
+++ codeminus/main/Application.php
@@ -4,18 +4,15 @@ namespace codeminus\main;
 
 use codeminus\file as file;
 
-require 'codeminus/main/Autoloader.php';
-Autoloader::init();
-
 /**
  * Codemius Framework Application 
  * @author Wilson Santos <wilson@codeminus.org>
- * @version 1.0
+ * @version 1.1
  */
 class Application {
 
-  use \codeminus\traits\Singleton;
-  
+  //use \codeminus\traits\Singleton;
+
   const DEV_MODE = 0;
   const PRO_MODE = 1;
 
@@ -27,6 +24,9 @@ class Application {
    * @return void
    */
   public static function init($mode = self::DEV_MODE, $configFile = null) {
+
+    require 'codeminus/main/Autoloader.php';
+    Autoloader::init();
 
     if (!isset($configFile)) {
       $ini = new file\Ini('app/config/main.ini');
@@ -50,15 +50,18 @@ class Application {
     }
 
     define('APP_PATH', $ini->get("path"));
-    Autoloader::includePath(APP_PATH);
+    define('APP_HTTP_PATH', $ini->get("http_path"));
+    define('APP_ENV_PATH', str_replace($_SERVER['DOCUMENT_ROOT'], '', APP_PATH));
     
+    Autoloader::includePath(APP_PATH);
+
     if (!is_dir(APP_PATH)) {
       throw new ExtendedException('Invalid environment directory.' . APP_PATH
       . ' does not exist.', E_ERROR);
     }
 
     define('ENV_MODE', $mode);
-    
+
     date_default_timezone_set($ini->get('timezone'));
 
     define('DB_HOST', $ini->get('db_host'));
@@ -67,29 +70,26 @@ class Application {
     define('DB_NAME', $ini->get('db_name'));
 
     $ini->setKeyPrefix(null);
-    
-    define('APP_ENV_PATH', str_replace($_SERVER['DOCUMENT_ROOT'], '', APP_PATH));
-    define('APP_HTTP_PATH', 'http://' . $_SERVER['HTTP_HOST'] . APP_ENV_PATH);
 
     define('VIEW_PATH', APP_PATH . $ini->get('view_dir'));
     define('VIEW_DEFAULT_TITLE', $ini->get('view_default_title'));
     define('VIEW_DEFAULT_HEADER', $ini->get('view_default_header'));
     define('VIEW_DEFAULT_FOOTER', $ini->get('view_default_footer'));
-    
+
     define('CONTROLLER_PATH', APP_PATH . $ini->get('controller_dir'));
     define('INDEX_CONTROLLER', $ini->get('controller_index'));
     define('ERROR_CONTROLLER', $ini->get('controller_error'));
-    
+
 
     define('CMF_PATH', APP_ENV_PATH . '/codeminus');
     define('CMF_ASSETS_PATH', CMF_PATH . '/assets');
     define('CMF_CSS_PATH', CMF_PATH . '/css');
     define('CMF_JS_PATH', CMF_PATH . '/js');
 
-    if(ENV_MODE == self::PRO_MODE){
-      file\File::create(APP_PATH.'/codeminus/run/.htaccess', 'deny from all');
-    }else{
-      file\File::delete(APP_PATH.'/codeminus/run/.htaccess');
+    if (ENV_MODE == self::PRO_MODE) {
+      file\File::create(APP_PATH . '/codeminus/run/.htaccess', 'deny from all');
+    } else {
+      file\File::delete(APP_PATH . '/codeminus/run/.htaccess');
     }
     
     //Initializing path router
@@ -101,4 +101,32 @@ class Application {
     }
   }
 
+  /**
+   * Returns the application root, considering that the framework will always
+   * be on app_root/codeminus folder
+   * @return string
+   */
+  public static function getRoot() {
+    return file\Directory::normalize(substr(__DIR__, 0,
+                            strpos(__DIR__, DIRECTORY_SEPARATOR . 'codeminus')));
+  }
+  
+  /**
+   * Returns the application http root, considering that the framework will
+   * always be on app_root/codeminus folder 
+   * @return string
+   */
+  public static function getHttpRoot() {
+    return 'http://'.$_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0,
+                    strpos($_SERVER['REQUEST_URI'], '/codeminus'));
+  }
+
+  /**
+   * Returns the path to the application's ini configuration file
+   * @return string
+   */
+  public static function getConfigPath(){
+    return file\Directory::normalize(self::getRoot()) . '/app/config/main.ini';
+  }
+  
 }
